2a133ca7f923e2f135e1957512fa6abf600b7c4b
Lucas Pillaga <lpillaga5@gmail.com>
2023-02-21 00:01:00 -0600

    player movement update
    I made player movement more realistic with drag and controlled speed however camera angle is off. Collision to be worked on.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,42 @@
-﻿using UnityEngine;
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10f; //Controls velocity multiplier
 
+    
+
     Rigidbody rb; //Tells script there is a rigidbody, we can use variable rb to reference it in further script
 
+    public Transform orientation;
     public float turnSpeed = 4.0f;
     public float moveSpeed = 2.0f;
     public float minTurnAngle = -90.0f;
     public float maxTurnAngle = 90.0f;
     private float rotX;
 
+    //groundCheck Drag
+    public float playerHeight;
+    public LayerMask whatisGround;
+    public float groundDrag;
+    bool grounded;
+
+
+    float horizontalInput;
+    float verticalInput;
+    Vector3 moveDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>(); 
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation=true;
+       // Cursor.lockState= CursorLockMode.Locked;
+        //Cursor.visible=false; 
     }
 
 
@@ -23,10 +44,41 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         MouseAiming();
+        //original code
 
-        float xMove = Input.GetAxisRaw("Horizontal"); 
-        float zMove = Input.GetAxisRaw("Vertical"); 
-        rb.velocity = new Vector3(xMove, rb.velocity.y, zMove) * speed; 
+        //ground check
+        grounded =Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f +0.2f, whatisGround);
+
+       //input 
+       horizontalInput = Input.GetAxisRaw("Horizontal"); 
+       verticalInput = Input.GetAxisRaw("Vertical"); 
+        speedControl();
+
+        //handle drag
+        if(grounded)
+            rb.drag= groundDrag;
+
+        else
+        {
+            rb.drag = 0;    
+        }
+
+
+
+    }
+    void FixedUpdate()
+    {
+        Moveplayer();
+
+    }
+
+    void Moveplayer()
+    {
+        moveDirection=orientation.forward *verticalInput +orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed *speed, ForceMode.Force);
+
+
+
     }
 
     void MouseAiming()
@@ -39,5 +91,25 @@ public class PlayerMovement : MonoBehaviour
         // rotate the camera
         transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + y, 0);
     }
+
+    void speedControl()
+{
+    Vector3 flatVel = new Vector3(rb.velocity.x,0f,rb.velocity.z);
+
+    if(flatVel.magnitude > moveSpeed)
+    {
+        Vector3 limitedVel =flatVel.normalized*moveSpeed;
+        rb.velocity= new Vector3(limitedVel.x,rb.velocity.y,limitedVel.z);
+
+
+    }
+
+
 }
+
+
+
+}
+
+
     
