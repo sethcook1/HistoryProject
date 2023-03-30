c47abd7d97247334176f672bd0627226730e4317
Ryan Morse <60400839+rjm5q@users.noreply.github.com>
2023-03-30 14:41:16 -0500

    UI Update
    Updated Item Menu to open at the bottom of the screen.
    Edited mouse_look script so when menu is opened the cursor is made available for use.

--- Assets/Scripts/mouse_look.cs
+++ Assets/Scripts/mouse_look.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class mouse_look : MonoBehaviour
 {
     public float mouseSens = 150f;
-
     public Transform playerBody;
-
+    public KeyCode menuButton;
+    public KeyCode exitButton;
     float xRotation = 0f;
+    private bool cursorLocked;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState=CursorLockMode.Locked;
-        
+        cursorLocked = true;
     }
 
     // Update is called once per frame
@@ -21,13 +23,29 @@ public class mouse_look : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        if (Input.GetKeyDown(menuButton) && cursorLocked)
+        {   
+            Cursor.lockState=CursorLockMode.None;
+            cursorLocked = false;   
+        }
+
+        if (Input.GetKeyDown(exitButton) && !cursorLocked)
+        {
+            Cursor.lockState=CursorLockMode.Locked;
+            cursorLocked = true;
+        }
         
 
         xRotation -= mouseY;
         xRotation= Mathf.Clamp(xRotation,-90f,90f);
 
-        transform.localRotation=Quaternion.Euler(xRotation,0f,0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        if (cursorLocked)
+        {
+            transform.localRotation=Quaternion.Euler(xRotation,0f,0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+        
 
 
     }
